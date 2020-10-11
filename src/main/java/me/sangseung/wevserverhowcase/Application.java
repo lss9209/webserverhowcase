1741a74c85e665a9e6fccd57c7b1a731996578e9
애플 <sangseunglee@aepeul-ui-MacBookPro.local>
2020-10-11 22:15:46 +0900

    Initial commit

---
+++ src/main/java/me/sangseung/wevserverhowcase/Application.java
@@ -0,0 +1,17 @@
+package me.sangseung.wevserverhowcase;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
